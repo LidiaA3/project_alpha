fce1651b0fb30fd8c1f8e38f1d43963e7a81591b
Lidia A <lidia.lasy@gmail.com>
2022-12-10 19:51:47 +0100

    First commit witho all elements

---
+++ js/basics.js
@@ -0,0 +1,25 @@
+const toggle_button = document.getElementById('js-button-toggle');
+const menu = document.getElementById('js-id-menu');
+
+function toggle_menu () {
+    if (menu.style.display === 'none') {
+        menu.style.display = 'flex';
+    } else {
+        menu.style.display = 'none';
+    }
+}
+
+const intro = document.getElementById('js-intro-container');
+
+function introDisplayNone () {
+    intro.style.display = 'none';
+}
+
+function introDisplayFlex () {
+    intro.style.display = 'flex';
+}
+
+if (intro.style.display === 'flex') {
+    alert('Hi! I am active');
+    toggle_button.style.stroke = "#faf5f4";
+}
