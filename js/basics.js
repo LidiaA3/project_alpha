cae4ca6b19a45ca7a7ce6b82f94f965c7d1b0572
Lidia A <lidia.lasy@gmail.com>
2022-12-13 22:22:10 +0100

    Media queries added and some styling changes

--- js/basics.js
+++ js/basics.js
@@ -1,23 +1,32 @@
 const toggle_button = document.querySelector('#js-button-toggle');
 const menu = document.querySelector('#js-id-menu');
 
+//function toggle_menu () {
+//    if (menu.style.display === 'none'|| menu.style.display === '') {
+//        menu.style.display = 'flex';
+//    } else {
+//        menu.style.display = 'none';
+//    }
+//}
+
 function toggle_menu () {
-    if (menu.style.display === 'none'|| menu.style.display === '') {
-        menu.style.display = 'flex';
-    } else {
-        menu.style.display = 'none';
-    }
+    menu.classList.toggle('display-flex');
 }
 
 const intro = document.querySelector('#js-intro-container');
 intro.style.display = 'flex';
 
+const body = document.querySelector('body');
+body.style.overflowY = 'hidden';
+
 function introDisplayNone () {
     intro.style.display = 'none';
     toggle_button.style.stroke = "#4b0000";
+    body.style.overflowY = 'scroll';
 }
 
 function introDisplayFlex () {
     intro.style.display = 'flex';
     toggle_button.style.stroke = "#faf5f4";
+    body.style.overflowY = 'hidden';
 }
